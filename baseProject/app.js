3a241e992785bf4ef3edee9854561d72e0757ec1
colinmyers37 <colinmyers37@gmail.com>
2022-11-02 14:18:34 -0500

    typescript objects

--- baseProject/app.js
+++ baseProject/app.js
@@ -1,7 +1,5 @@
-function add(n1, n2) {
-    return n1 + n2;
-}
-var number1 = '5';
-var number2 = 3;
-var result = add(number1, number2);
-console.log(result);
+var person = {
+    name: 'Colin',
+    age: 21
+};
+console.log(person.name);
