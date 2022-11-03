31e2752ec69802e3ba3e10b6a96fbd9e57bc823f
colinmyers37 <colinmyers37@gmail.com>
2022-11-03 03:17:21 -0500

    return types and 'void'

--- baseProject/app.js
+++ baseProject/app.js
@@ -1,21 +1,7 @@
-function combine(input1, input2, resultConversion) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    if (resultConversion === 'as-number') {
-        return +result;
-    }
-    else {
-        return result.toString();
-    }
-    return result;
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combineAges = combine(30, 26, 'as-number');
-var combineStringAges = combine('30', '26', 'as-number');
-var combineNames = combine('max', 'anna', 'as-text');
-console.log(combineAges);
-console.log(combineStringAges);
+function printResult(number) {
+    console.log('Result: ' + number);
+}
+printResult(add(5, 12));
---
+++ baseProject/app.ts
@@ -0,0 +1,9 @@
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+function printResult(number: number): void {
+  console.log('Result: ' + number);
+}
+
+printResult(add(5, 12));
