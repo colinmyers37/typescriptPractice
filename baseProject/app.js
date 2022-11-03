856e45f84d7329077460e69064e1df68cb071953
colinmyers37 <colinmyers37@gmail.com>
2022-11-03 02:33:39 -0500

    tuples practice

--- baseProject/app.js
+++ baseProject/app.js
@@ -1,5 +1,16 @@
+// const person: {
+//     name: string;
+//     age: number;
+// }
 var person = {
     name: 'Colin',
-    age: 21
+    age: 21,
+    hobbies: ['Sports', 'Cooking']
 };
+var favoriteActivities;
+favoriteActivities = ['sport'];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
