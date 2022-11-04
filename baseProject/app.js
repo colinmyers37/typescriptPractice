81cc4911012619ec8e6bc2fa3d451a8c86a2832f
colinmyers37 <colinmyers37@gmail.com>
2022-11-04 02:13:40 -0500

    function types

--- baseProject/app.js
+++ baseProject/app.js
@@ -4,4 +4,15 @@ function add(n1, n2) {
 function printResult(number) {
     console.log('Result: ' + number);
 }
+function addHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
 printResult(add(5, 12));
+var combineValues;
+combineValues = add;
+// combineValues = printResult;
+console.log(combineValues(8, 8));
+addHandle(10, 20, function (result) {
+    console.log(result);
+});
